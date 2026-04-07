4ff53f5a1cfe11fdee8b8293df5a2d0fad7a4442
Dhivshan <dhivyalshan@gmail.com>
2026-04-07 17:59:27 +0530

    Add files via upload
    Initial Schema SQL File

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE metro_pulse.delhi_metro_trips (
+    trip_id INT PRIMARY KEY,
+    date DATETIME NOT NULL,
+    from_station VARCHAR(100) NOT NULL,
+    to_station VARCHAR(100) NOT NULL,
+    distance_km DECIMAL(10,2) NOT NULL,
+    fare DECIMAL(6,2) NOT NULL,
+    cost_per_passenger DECIMAL(10,2) NOT NULL,
+    passenger INT NOT NULL DEFAULT 0,
+    ticket_type VARCHAR(50) NOT NULL,
+    remarks VARCHAR(100) NOT NULL
+);
